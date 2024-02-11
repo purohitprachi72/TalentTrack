a952ec26531881fd2f4d0892273c3b910a9d8510
Dheeraj Purohit <purohitdheeraj89@gmail.com>
2024-02-11 18:45:20 +0530

    feat : add delete

--- src/api.js
+++ src/api.js
@@ -1,15 +1,28 @@
+import axios from 'axios';
 import { BASEURL } from './constants/constant';
 
 export async function getCandidateData(id) {
 	const url = id ? `${BASEURL}/${id}` : BASEURL;
 
-	const response = await fetch(url);
-	if (!response.ok) {
-		const error = new Error('Failed to Fetch');
-		error.status = response.status;
-		error.statusText = response.statusText;
+	try {
+		const response = await axios.get(url);
+
+		return response.data;
+	} catch (error) {
+		console.error('Error fetching candidate data:', error);
 		throw error;
 	}
-	const parseResponse = await response.json();
-	return parseResponse;
+}
+
+export async function deleteCandidate(id) {
+	const url = `${BASEURL}/${id}`;
+
+	try {
+		const response = await axios.delete(url);
+
+		return response.data;
+	} catch (error) {
+		console.error('Error deleting candidate:', error);
+		throw error;
+	}
 }
